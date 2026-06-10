97e3ac01c4ba166855ef390e6bacdacb86ee4edd
prabhakarkoranga41 <prabhakarkoranga41@gmail.com>
2026-06-10 16:11:42 +0530

    UserProfile Dto changes

--- TrackNest.Application/DTOs/UserProfileDto.cs
+++ TrackNest.Application/DTOs/UserProfileDto.cs
@@ -3,20 +3,20 @@ namespace TrackNest.Application.DTOs
     public class UserProfileDto
     {
         public int Id { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
     }
 
     public class UserLoginDto
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 
     public class UserSignupDto
     {
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
